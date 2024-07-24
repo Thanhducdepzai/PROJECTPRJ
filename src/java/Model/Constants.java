a710ac15bbb0e8e96d0a482f96903c0cf46b6de3
Thanhducdepzai <136815568+Thanhducdepzai@users.noreply.github.com>
2024-07-24 18:25:21 +0700

    Remove secrets from Constants.java

---
+++ src/java/Model/Constants.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author dell
+ */
+public class Constants {
+
+    public final static String GOOGLE_CLIENT_ID = "your-client-id";
+
+    public final static String GOOGLE_CLIENT_SECRET = "your-client-secret";
+
+    public final static String GOOGLE_REDIRECT_URI = "http://localhost:9999/loginEmail";
+
+    public final static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+
+    public final static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+
+    public final static String GOOGLE_GRANT_TYPE = "authorization_code";
+
+    public final static boolean DeleteFalse = false;
+
+    public final static boolean DeleteTrue = true;
+
+    public final static boolean Active = true;
+    
+    public final static boolean Deactive = false;
+    
+    public final static boolean Parent = true;
+    
+    public final static boolean Children = false;
+}
